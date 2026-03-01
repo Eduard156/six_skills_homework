41da9b57198e2fccdba923f20cad5c149d875ae2
ed <thegreenwolf156@gmail.com>
2026-03-01 22:54:56 +0300

    реализовано решение к практическому заданию 5

--- src/test/scala/perf/load/scenarios/HttpSherlockScenario.scala
+++ src/test/scala/perf/load/scenarios/HttpSherlockScenario.scala
@@ -3,6 +3,7 @@ package perf.load.scenarios
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import perf.load.cases.HttpSherlock
+import perf.load.cases.HttpSherlock.{credFeeder, logger}
 
 object HttpSherlockScenario {
   def apply(): ScenarioBuilder = new HttpSherlockScenario().scn
@@ -11,7 +12,20 @@ object HttpSherlockScenario {
 class HttpSherlockScenario {
 
   val scn: ScenarioBuilder = scenario("Http Sherlock Scenario")
+    .feed(credFeeder)
     .exec(HttpSherlock.postToken)
-    .exec(HttpSherlock.getProfile)
+    .exec { session =>
+      {
+        val login  = session("username").as[String]
+        val status = session("responseStatus").as[Int]
+
+        if (status == 200)
+          logger.info(s"[Success] Username: $login")
+        else
+          logger.warn(s"[FAIL] Username: $login")
+      }
+      session
+    }
+//    .exec(HttpSherlock.getProfile)
 
 }
